b8fd532326593de50b9a05c51e2c735c85f325cf
[isabelleedlund] <[edlund.isabelle@gmail.com]>
2020-08-24 11:53:26 +0200

    changes the schema to have bookingschema and userschema

--- model/userinfo.ts
+++ model/userinfo.ts
@@ -1,15 +1,20 @@
 import { createSchema, Type, typedModel } from 'ts-mongoose';
 
-export const UserSchema = createSchema({
-        customerid: Type.number ({ required: true }), // Tror detta skapas av sig själv?
-        firstname: Type.string ({ required: true }),
-        lastname: Type.string ({ required: true }),
-        email: Type.string ({ required: true }),
-        phone: Type.string ({ required: true }),
-        comment: Type.string ({ required: true }),
-        date: Type.date ({ default: Date.now as any }),
-        timeslot: Type.string ({ required: true }), // Är inte säker på om timesloten är en string eller number?
-        qty: Type.number ({ required: true })
-    })
+const UserSchema = createSchema({
+    customerid: Type.number ({ required: true }), // Tror detta skapas av sig själv?
+    firstname: Type.string ({ required: true }),
+    lastname: Type.string ({ required: true }),
+    email: Type.string ({ required: true }),
+    phone: Type.string ({ required: true })
+});
 
-    export const User = typedModel('User', UserSchema);
+
+const BookingSchema = createSchema({
+    date: Type.date ({ default: Date.now as any }),
+    timeslot: Type.string ({ required: true }),
+    qty: Type.number ({ required: true }),
+    comment: Type.string ({ required: true })
+});
+
+export const User = typedModel('User', UserSchema);
+export const Booking = typedModel('Booking', BookingSchema);
