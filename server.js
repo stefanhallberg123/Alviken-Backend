86d62ee6744c6ec13586c43ec0d627599a1d52f4
[stefanhallberg123] <[stefanhalllberg@live.se]>
2020-08-24 12:13:33 +0200

    changing from ts to js

--- server.js
+++ server.js
@@ -7,13 +7,16 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(cors());
 app.use(express.json());
+
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology: true,
 });
 const connection = mongoose.connection;
@@ -22,9 +25,6 @@ connection.once("open", () => {
 });
 
 // lägg till Routes här och app.use på dessa
-const userForm = require("./routes/userForm");
-
-app.use("/", userForm);
 
 app.listen(port, () => {
     console.log(`server is running on ${port}`);
