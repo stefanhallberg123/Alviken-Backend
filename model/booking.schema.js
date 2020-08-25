878031f385f4656cc9cfac4c3a39d13952a0f6b1
[isabelleedlund] <[edlund.isabelle@gmail.com]>
2020-08-25 13:21:10 +0200

    changes that Stefan did to Schema

--- model/booking.schema.js
+++ model/booking.schema.js
@@ -1,37 +1,20 @@
 const mongoose = require("mongoose");
 
-// const bookingSchema = new mongoose.Schema({
-//     user: {
-//         name: { type: String, required: true },
-//         email: { type: String, required: true },
-//         phone: { type: String, required: true },
-//         customerId: { type: Number }
-//     },
-//     booking: {
-//         comment: { type: String },
-//         date: { type: Date, required: true },
-//         timeslot: { type: String, required: true },
-//         qty: { type: String, required: true },
-//     },
-//     id: { type: String }
-// });
-const bookingSchema = new mongoose.Schema({
+const Booking = new mongoose.Schema({
+    comment: { type: String },
+    date: { type: Date, },
+    timeslot: { type: String, },
+    qty: { type: String },
+    table: { type: Number, min: 1, max: 15 }, // veta antalet bord? 
     user: {
         name: { type: String },
         email: { type: String },
         phone: { type: String },
         customerId: { type: Number }
     },
-    booking: {
-        comment: { type: String },
-        date: { type: Date },
-        timeslot: { type: String },
-        qty: { type: String },
-    },
-    id: { type: String }
 });
 
 
-const Booking = mongoose.model("booking", bookingSchema)
+const BookingSchema = mongoose.model("booking", Booking)
 
-module.exports = Booking;
+module.exports = BookingSchema;
