a7f05f4fa4ee08813d28c7813f6b4251b5f74166
[stefanhallberg123] <[stefanhalllberg@live.se]>
2020-08-25 10:46:35 +0200

    cleaning upp some

--- model/booking.schema.js
+++ model/booking.schema.js
@@ -1,22 +1,5 @@
 const mongoose = require("mongoose");
 
-
-// const bookingSchema = new mongoose.Schema({
-//     user: {
-//         name: { type: String, required: true },
-//         email: { type: String, required: true },
-//         phone: { type: String, required: true },
-//         customerId: { type: Number }
-//     },
-//     booking: {
-//         comment: { type: String },
-//         date: { type: Date, required: true },
-//         timeslot: { type: String, required: true },
-//         qty: { type: String, required: true },
-//     },
-//     id: { type: String }
-// });
-
 const Booking = new mongoose.Schema({
     comment: { type: String },
     date: { type: Date },
