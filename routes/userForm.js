2c70a211754f0ffe4bdc9af21d63146a17ca78dc
[stefanhallberg123] <[stefanhalllberg@live.se]>
2020-08-31 15:42:43 +0200

    changed the customerId to mongoDb id

--- routes/userForm.js
+++ routes/userForm.js
@@ -3,30 +3,28 @@ const router = require("express").Router();
 let Booking = require("../model/booking.schema");
 
 router.get("/", async (req, res) => {
-    const booking = await Booking.find();
+  const booking = await Booking.find();
 });
 
 router.post("/", async (req, res) => {
-
-    const booking = await new Booking({
-        table: 1, // antal som läggs till?
-        comment: req.body.comment,
-        date: req.body.date,
-        timeslot: req.body.timeslot,
-        qty: req.body.qty,
-        user: {
-            name: req.body.name,
-            email: req.body.email,
-            phone: req.body.phone,
-            customerId: req.body.customerId
-        }
-    }).save();
-    console.log(booking);
-})
+  const booking = await new Booking({
+    table: 1, // antal som läggs till?
+    comment: req.body.comment,
+    date: req.body.date,
+    timeslot: req.body.timeslot,
+    qty: req.body.qty,
+    user: {
+      name: req.body.bookingUser.name,
+      email: req.body.bookingUser.email,
+      phone: req.body.bookingUser.phone,
+      // customerId: req.body.customerId,
+    },
+  }).save();
+  console.log(booking);
+});
 
 router.get("/thankyou", async (req, res) => {
-    const orderNumber = await Booking.findOne({ orderNumber: req.body._id })
-})
-
+  const orderNumber = await Booking.findOne({ orderNumber: req.body._id });
+});
 
 module.exports = router;
