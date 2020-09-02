82f57fcb219885ffa67eab758adcea716effe19f
[stefanhallberg123] <[stefanhalllberg@live.se]>
2020-09-02 10:57:46 +0200

    finished the admin-routes

--- routes/userForm.js
+++ routes/userForm.js
@@ -24,7 +24,46 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/thankyou", async (req, res) => {
-  const orderNumber = await Booking.findOne({ orderNumber: req.body._id });
+  const orderNumber = await Booking.findOne({
+    orderNumber: req.body._customerId,
+  });
 });
+router.get("/admin", async (req, res) => {
+  const booking = await Booking.find();
+});
+router.post("/admin/create", async (req, res) => {
+  const booking = await new Booking({
+    table: 1, // antal som läggs till?
+    comment: req.body.comment,
+    date: req.body.date,
+    timeslot: req.body.timeslot,
+    qty: req.body.qty,
+    user: {
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      // customerId: req.body.customerId,
+    },
+  }).save();
+  console.log(booking);
+});
+router.post("admin/edit/id", async (req, res) => {
+  const booking = await Booking.updateOne({ _id: req.body._customerId })
+  booking.comment = req.body.comment,
+   booking.date =req.body.date,
+    booking.timeslot = req.body.timeslot,
+    booking.qty = req.body.qty,
+    booking.user = {
+      name = req.body.name,
+      email: req.body.email,
+      phone: req.body.phone
+    }, await booking.save()
+    console.log(booking);
+  });
+  router.post("admin/delete/id", async(req, res ) => {
+    await Booking.deleteOne({
+      _id: req.params._customerId
+    })
+  });
 
 module.exports = router;
